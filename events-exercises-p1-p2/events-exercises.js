1bf6925a0bb3eebd26298ebb9647c8ec30cd00f8
removgeorgi <remov.georgi@gmail.com>
2020-04-04 14:20:23 +0300

    some refactoring + part2 of events exercises - ADDED file

--- events-exercises-p1-p2/events-exercises.js
+++ events-exercises-p1-p2/events-exercises.js
@@ -1,13 +1,15 @@
-//PART I
+// PART I
 
-//1.Add the necessary code to wait for the DOM to load to make sure that anything you manipulate in the DOM has loaded. 
-//You can do this either using window.onload or adding an event listener for DOMContentLoaded.
-//2.Replace the text "Change me" with "Hello World!".
+// 1.Add the necessary code to wait for the DOM to load to make sure that anything you manipulate 
+// in the DOM has loaded. 
+// You can do this either using window.onload or adding an event listener for DOMContentLoaded.
+// 2.Replace the text "Change me" with "Hello World!".
 window.onload = function(){
     let heading = document.getElementById("change_heading");
     heading.innerText = "Hello World!";
 
-//3.When a user hovers over one of the colored boxes change the text to display the color that is being hovered over.
+// 3.When a user hovers over one of the colored boxes change the text 
+// to display the color that is being hovered over.
 
     let section = document.querySelector("section");
 section.addEventListener("mouseover", function(event) {
@@ -16,21 +18,21 @@ section.addEventListener("mouseover", function(event) {
 });
 
 
-//4.Create a new div element.
+// 4.Create a new div element.
 
 let newDiv = document.createElement("div");
 
-//5.Give your new div a class of purple and style it so that it has a background color of purple.
+// 5.Give your new div a class of purple and style it so that it has a background color of purple.
 newDiv.className = "purple";
 
-//6.Append your new div to the page to the section tag.
+// 6.Append your new div to the page to the section tag.
 section.appendChild(newDiv);
 
 
-//PART II
-//Create a racing game with the two cars. When the race button is pressed, 
-//the two cars should move across the screen until one of them is at the end of the screen. 
-//When one of the blocks reaches the end - you should alert "winner!"
+// PART II
+// Create a racing game with the two cars. When the race button is pressed, 
+// the two cars should move across the screen until one of them is at the end of the screen. 
+// When one of the blocks reaches the end - you should alert "winner!"
 
 let button = document.querySelector("button");
 let car1 = document.querySelector('.car1');
